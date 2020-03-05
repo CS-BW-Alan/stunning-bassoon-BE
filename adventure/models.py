74ba7c59fdbc4521a8b3fa902d2f4a3ea711692c
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-03-05 13:28:59 -0800

    work on game class and queue; will probably be sidelined for now

--- adventure/models.py
+++ adventure/models.py
@@ -6,6 +6,7 @@ from rest_framework.authtoken.models import Token
 import uuid
 import random
 
+
 def randomint():
     return random.randint(1,10) * 10
 
@@ -20,6 +21,8 @@ class Room(models.Model):
     e_to = models.IntegerField(default=0)
     w_to = models.IntegerField(default=0)
     points = models.IntegerField(default=randomint)
+    game = models.OneToOneField(Game, on_delete=models.CASCADE)
+
     def connectRooms(self, destinationRoom, direction):
         destinationRoomID = destinationRoom.id
         try:
@@ -51,6 +54,9 @@ class Player(models.Model):
     uuid = models.UUIDField(default=uuid.uuid4, unique=True)
     moves = models.IntegerField(default=0)
     points = models.IntegerField(default=0)
+    game = models.OneToOneField(Game, on_delete=models.CASCADE)
+    queueNumber = models.IntegerField(default=0)
+
     def initialize(self):
         if self.currentRoom == 0:
             self.currentRoom = Room.objects.first().id
@@ -61,6 +67,58 @@ class Player(models.Model):
         except Room.DoesNotExist:
             self.initialize()
             return self.room()
+
+class Queue(models.Model):
+    game = models.OneToOneField(Game, on_delete=models.CASCADE)
+    player = models.OneToOneField(Player, on_delete=models.CASCADE)
+    number = models.IntegerField(default=0)
+
+    def get(self):
+        next_player = Queue.objects.filter(game=self.game).filter(number=0)
+        # next_player.number = 
+
+
+class Game(models.Model):
+    pointsRemaining = models.IntegerField(default=0)
+    inSession = models.BooleanField(default=False)
+
+    # TODO:
+    # - Add game to Room, along with everywhere room is instatiated (eg map_generator.py)
+    # - Make multiple pusher channels: universal, and individiual game
+
+    def initialize(self):
+        self.inSession = True
+
+    def join(self, player):
+        player.game = self.id
+        queue = Queue.objects.filter(game=self.id)
+        queue.put(player)
+
+    def getPoints(self, player, room):
+        points = room.points
+        room.points = 0
+        player.points += points
+        self.pointsRemaining -= points
+        
+        # Check for endgame:
+        if self.pointsRemaining == 0:
+            pusher = Pusher(app_id=config('PUSHER_APP_ID'), key=config('PUSHER_KEY'), secret=config('PUSHER_SECRET'), cluster=config('PUSHER_CLUSTER'))
+            self.inSession = False
+            pusher.trigger('game-channel', 'endgame', {'winner': player})
+    
+    def runGame(self):
+        # queue = Queue()
+        winner = None
+
+        # Add players to queue
+        for player in Player.objects.filter(game=self.id):
+            # queue.put(player)
+        
+        # Main loop
+        while winner == None:
+
+
+
 
 @receiver(post_save, sender=User)
 def create_user_player(sender, instance, created, **kwargs):
