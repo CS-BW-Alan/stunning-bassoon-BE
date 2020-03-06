15ded6f048419d7879384970fd75cea0eff17a06
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2020-03-06 11:25:44 -0800

    added comments for pusher stuff in join and leave

--- adventure/api.py
+++ adventure/api.py
@@ -133,15 +133,17 @@ def joinGame(request):
     newPlayer.save()
     # add logic: player drops in room
 
-
+    # Pusher
+    # Updated player list to pass through pusher
     players = [{
         "player_id": p.id,
         "username": p.user.username,
         "score": p.points,
         "current_room": p.currentRoom,
     } for p in Player.objects.all()]
-
+    # Trigger player-joined event and pass in updated players list
     pusher.trigger('player-channel', 'player-joined', {'message': f"{request.user.username} has joined the game", 'player': user.username, 'players': players})
+    
     return JsonResponse({'Msg':"Join Successful"}, safe=True)
 
 @api_view(["GET"])
@@ -154,16 +156,17 @@ def leaveGame(request):
     except Player.DoesNotExist:
         return JsonResponse({'error_msg':"Player has already left."}, safe=True)
 
-
+    # Pusher
+    # Updated player list to pass through pusher
     players = [{
         "player_id": p.id,
         "username": p.user.username,
         "score": p.points,
         "current_room": p.currentRoom,
     } for p in Player.objects.all()]
-
-
+    # Trigger player-left event and pass in updated players list
     pusher.trigger('player-channel', 'player-left', {'message': f"{request.user.username} has left the game", 'player': user.username, 'players': players})
+
     return JsonResponse({'Msg':"Leave Successful"}, safe=True)
 
 import random
